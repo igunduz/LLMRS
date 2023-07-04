6adae30c649143fc47e7df28e3a11db786db7745
theo <thai00001@uni-saaraland.de>
2023-07-04 22:36:06 +0200

    Reading data from api

--- src/recommendation_api.py
+++ src/recommendation_api.py
@@ -1,42 +1,48 @@
-from flask import Flask, render_template, request, jsonify, make_response
-import requests
-import sys
 import os
-from os.path import join, exists
-import openai
-from transformers import pipeline, Conversation
+import sys
+from os.path import join
+
+from flask import Flask, render_template, request, jsonify, send_from_directory
+
 sys.path.append(join(os.getcwd(), 'src'))
-from logger import logger
 from software_recommender import Zero_Shot_Recosys
 
 
-api_key = "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"
-openai.api_key = api_key
-
-# webapp
 app = Flask(__name__, template_folder='web_interface')
 
 reviews_path = "data/reviews.csv"
 software_path = "data/softwares.csv"
 query_params = {
-"software_description" : "HR program for windows and mac for japanese humans",
-"max_price" : 500,
-"min_price" : 10
+    "software_description": "HR program for windows and mac for japanese humans",
+    "max_price": 500,
+    "min_price": 10
 }
 obj = Zero_Shot_Recosys(reviews_path, software_path)
+
 
 @app.route("/")
 def home():
     return render_template("home.html")
-@app.route('/recommender')
+
+@app.route('/image/<path:filename>')
+def serve_image(filename):
+    return send_from_directory('static', filename)
+@app.route('/recommender', methods=['GET'])
 def recommend():
-    data = request.get_json()
-    print(data)
+    query_params = {
+        "software_description": request.args.get('software_description', ''),
+        "max_price": request.args.get('max_price', ''),
+        "min_price": request.args.get('min_price', ''),
+        "max_license": request.args.get('max_license', ''),
+        "min_license": request.args.get('min_license', ''),
+        "max_maintenance": request.args.get('max_maintenance', ''),
+        "min_maintenance": request.args.get('min_maintenance', '')
+    }
+
     output = obj.recommender(query_params)
-    out_data = output.to_json(orient="records")
-    print(out_data)
+    out_data = output.to_json(orient="index")
     return jsonify({"output": out_data})
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=True)
