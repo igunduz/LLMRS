5ab5ed7096d369d9fb61135c1f698946c93d6f80
theo <thai00001@uni-saaraland.de>
2023-07-05 22:08:52 +0200

    Testing

--- src/recommendation_api.py
+++ src/recommendation_api.py
@@ -1,32 +1,25 @@
 import os
 import sys
 from os.path import join
+import json
 
-from flask import Flask, render_template, request, jsonify, send_from_directory
+import pandas as pd
+from flask import Flask, render_template, request, jsonify
 
 sys.path.append(join(os.getcwd(), 'src'))
 from software_recommender import Zero_Shot_Recosys
 
+software_path = "data/softwares_with_score.csv"
+softwares = pd.read_csv(software_path)
+obj = Zero_Shot_Recosys()
 
 app = Flask(__name__, template_folder='web_interface')
-
-reviews_path = "data/reviews.csv"
-software_path = "data/softwares.csv"
-query_params = {
-    "software_description": "HR program for windows and mac for japanese humans",
-    "max_price": 500,
-    "min_price": 10
-}
-obj = Zero_Shot_Recosys(reviews_path, software_path)
-
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
-@app.route('/image/<path:filename>')
-def serve_image(filename):
-    return send_from_directory('static', filename)
+
 @app.route('/recommender', methods=['GET'])
 def recommend():
     query_params = {
@@ -39,9 +32,9 @@ def recommend():
         "min_maintenance": request.args.get('min_maintenance', '')
     }
 
-    output = obj.recommender(query_params)
-    out_data = output.to_json(orient="index")
-    return jsonify({"output": out_data})
+    output = obj.recommender(softwares, query_params)
+    output = output.to_json(orient='index')
+    return jsonify({"output": output})
 
 
 if __name__ == '__main__':
